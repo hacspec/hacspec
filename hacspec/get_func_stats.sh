7cd28b68494f3d5a06d058e1655860d120fd0205
Denis Merigoux <denis.merigoux@gmail.com>
2020-04-06 18:30:10 +0200

    Added counter

---
+++ hacspec/get_func_stats.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "Primitive functions:"
+cargo build --features="print_attributes" 2>&1 | grep --color=always "Primitive" | wc -l
+echo "External functions:"
+cargo build --features="print_attributes" 2>&1 | grep --color=always "External" | wc -l
+echo "Library functions:"
+cargo build --features="print_attributes" 2>&1 | grep --color=always "Library" | wc -l
+echo "To remove functions:"
+cargo build --features="print_attributes" 2>&1 | grep --color=always "To remove" | wc -l
