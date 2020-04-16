d049f079584bc4c847652badad26df6e1c801df2
Franziskus Kiefer <franziskuskiefer@gmail.com>
2020-04-16 09:31:33 +0200

    fix get_func_stats.sh

--- hacspec/get_func_stats.sh
+++ hacspec/get_func_stats.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 echo "Primitive functions:"
-cargo build --features="use_attributes" 2>&1 | grep --color=always "Primitive" | wc -l
+cargo +nightly build --features="use_attributes" 2>&1 | grep --color=always "Primitive" | wc -l
 echo "External functions:"
-cargo build --features="use_attributes" 2>&1 | grep --color=always "External" | wc -l
+cargo +nightly build --features="use_attributes" 2>&1 | grep --color=always "External" | wc -l
 echo "Library functions:"
-cargo build --features="use_attributes" 2>&1 | grep --color=always "Library" | wc -l
+cargo +nightly uild --features="use_attributes" 2>&1 | grep --color=always "Library" | wc -l
 echo "To remove functions:"
-cargo build --features="use_attributes" 2>&1 | grep --color=always "To remove" | wc -l
+cargo +nightly build --features="use_attributes" 2>&1 | grep --color=always "To remove" | wc -l
 echo "Internal functions:"
-cargo build --features="use_attributes" 2>&1 | grep --color=always "Internal" | wc -l
+cargo +nightly build --features="use_attributes" 2>&1 | grep --color=always "Internal" | wc -l
