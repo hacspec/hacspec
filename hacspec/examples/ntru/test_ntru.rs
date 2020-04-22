dbe728d273b91de0205775ac7c0c55a857cd0922
Tomer Y <44681727+TomerHawk@users.noreply.github.com>
2020-04-22 14:43:24 +0200

    Add files via upload

--- hacspec/examples/ntru/test_ntru.rs
+++ hacspec/examples/ntru/test_ntru.rs
@@ -9,3 +9,23 @@ fn test_key_gen(){
     //assert_eq!(p,5);
     //assert_eq!(g,g_inv);
 }
+#[test]
+fn test_create_inv(){
+    let mut poly = create_invertable_poly_2( NtruVersion{p:653,q:4621,w:288},3);
+    while poly.1.len() == 0 {
+        poly = create_invertable_poly_2( NtruVersion{p:653,q:4621,w:288}, 3);
+    }
+    // convert vec to array
+    let mut f_array:[u128;653] = [0;653];
+    let mut index = 0;
+    while index < 653{
+            f_array[index] = poly.1[index];
+            index = index + 1;
+    }
+    
+    poly!(ZxQ, u128, 653, 3, [(0, 2), (1, 2), (653, 1)]);
+    let f_inv = ZxQ::new_full(f_array);
+    let f = ZxQ::new(&poly.0);
+    let result = ZxQ::new(&[(0,1)]);
+    assert_eq!(result,f.mul(f_inv) );
+}
