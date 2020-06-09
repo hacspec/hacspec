23f3e86b80b1c38771cc720c9f7878568219bdef
Tomer Y <44681727+TomerHawk@users.noreply.github.com>
2020-06-09 15:39:33 +0200

    Add files via upload

--- spec-examples/tests/test_ntru.rs
+++ spec-examples/tests/test_ntru.rs
@@ -29,3 +29,68 @@ fn test_create_inv() {
     );
     assert_eq!(true,true);
 }
+
+fn test_h_invertable(h:&Seq<u128>,q:u128)-> bool{
+    let mut irr:Seq<u128> = Seq::new(h.len());
+    irr[0] = q-1;
+    irr[1] = q-1;
+    irr[h.len() -1] = 1 as u128;
+
+
+    let h_pre_inv = eea(&h,&irr,q);
+    let mut h_inv:Seq<u128> = Seq::new(h.len());
+    match h_pre_inv {
+        Ok(v) => {
+            h_inv = v;
+        }
+        Err(_) => println!("Key generating, failed")
+    }
+    if deg(&h_inv) != 0{
+        return true;
+    }
+    return false;
+    }
+
+#[test]
+fn test_encryption_decryption() {
+    let n_v = ntru_v!(-1);
+    let keys = key_gen(&n_v);
+    println!("{:?}",keys);
+    let pk = keys.0;
+    let sk = keys.1;
+
+    // message
+    let m = create_rand_poly(n_v.w,n_v.p + 1 ,3);
+    // encryption
+    let c = encryption(&m, pk, &n_v);
+    println!("encryption done!");
+    println!("c is {:?}", c);
+    let result = decryption(c, sk, &n_v);
+    assert_eq!(deg(&result),deg(&m));
+    println!("Compare weight:");
+    assert_eq!(weight(&result),weight(&m));
+    println!("m is {:?} and result is {:?}",m, result);
+}
+
+#[test]
+fn test_encryption_decryption_fixed_key(){
+    let n_v = ntru_v!(-1);
+    let pk:Seq<u128> = Seq::from_slice(&[26, 39, 52, 91, 13, 39, 52, 0]);
+    let sk:(Seq<u128>,Seq<u128>) = (Seq::from_slice(&[0, 1, 2, 1, 2, 0, 0, 0]),Seq::from_slice(&[0, 0, 0, 1, 0, 0, 2, 0]));
+    println!("h is invertable? {:?}",test_h_invertable(&pk, n_v.q) );
+    assert_eq!(test_h_invertable(&pk, n_v.q),true);
+    // message
+    let m:Seq<u128> = Seq::from_slice(&[2, 2, 1, 0, 0, 2, 0, 0]);
+    // encryption
+    let c = encryption(&m, pk, &n_v);
+    println!("c is {:?}",c);
+    println!("encryption done!");
+    let result = decryption(c, sk, &n_v);
+    println!("compare degree: ");
+    assert_eq!(deg(&result),deg(&m));
+
+    println!("Compare weight:");
+    assert_eq!(weight(&result),weight(&m));
+
+    println!("m is {:?} and result is {:?}",m, result);
+}
