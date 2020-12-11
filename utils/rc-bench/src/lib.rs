49caa425795a49bf51574086ed4064daa3f59352
Franziskus Kiefer <franziskuskiefer@gmail.com>
2020-12-11 09:29:21 +0100

    provider part 2 (#58)
    * fix benchmarks in unsafe examples
    * add provider benchmarks
    * add RustCrypto Chacha20Poly1305 to tests and benchmarks

---
+++ utils/rc-bench/src/lib.rs
@@ -0,0 +1,8 @@
+//! # Provider benchmarks
+//!
+//! This is a dummy crate that implements benchmarks on the [RustCrypto traits](https://github.com/RustCrypto/traits)
+//! API for different providers.
+//!
+//! Tested providers:
+//! * hacspec
+//! * evercrypt
