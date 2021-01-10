2f9e8856b3c605d8710082b54ee5899fe0387224
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2021-01-10 09:33:02 +0000

    deploy: b2de64a060ea224a0500cdaab82433951703b856

---
+++ hacspec_lib/seq/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["PublicSeq","Variable length byte arrays."],["SecretSeq","Variable length byte arrays."],["Seq","Variable length byte arrays."]],"type":[["ByteSeq",""],["PublicByteSeq",""]]});
