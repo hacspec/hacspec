0e960e62faa464e2ed5a09b41000fb7349f60c9e
denismerigoux <denismerigoux@users.noreply.github.com>
2021-05-04 16:37:26 +0000

    deploy: 756b4d9c3b3c11e0f136be945053787934c2db24

---
+++ implementors/hacspec_provider/trait.AeadCore.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["hacspec_provider"] = [{"text":"impl <a class=\"trait\" href=\"hacspec_provider/trait.AeadCore.html\" title=\"trait hacspec_provider::AeadCore\">AeadCore</a> for <a class=\"struct\" href=\"hacspec_provider/struct.Chacha20Poly1305.html\" title=\"struct hacspec_provider::Chacha20Poly1305\">Chacha20Poly1305</a>","synthetic":false,"types":["hacspec_provider::provider::Chacha20Poly1305"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
