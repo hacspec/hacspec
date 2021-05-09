b326d7d6526fa8f33b2c5ee5a7fe0ba648e79c34
karthikbhargavan <karthikbhargavan@github.com>
2021-05-09 11:03:22 +0200

    bulk speedup

--- protocols/tls13/src/tls13record.rs
+++ protocols/tls13/src/tls13record.rs
@@ -17,16 +17,17 @@ pub fn derive_iv_ctr(ae: &AEADAlgorithm, iv: &AEIV, n:u64) -> AEIV {
     iv_ctr
 }
 
-pub fn encrypt_record_payload(ae:&AEADAlgorithm, kiv: &AEKIV, n:u64, ct:ContentType, payload: &Bytes, pad:usize) -> Res<Bytes> {
+pub fn encrypt_record_payload(ae:&AEADAlgorithm, kiv: &AEKIV, n:u64, ct:ContentType, payload: Bytes, pad:usize) -> Res<Bytes> {
     let (k,iv) = kiv;
     let iv_ctr = derive_iv_ctr(&ae,&iv,n);
-    let inner_plaintext = payload.concat(&bytes1(content_type(ct))).concat(&zeros(pad));
+    let inner_plaintext = payload.concat_owned(bytes1(content_type(ct))).concat_owned(zeros(pad));
     let clen = inner_plaintext.len() + 16;
     if clen <= 65536 {
-        let clenb = u16_to_be_bytes(clen as u16);
-        let ad = bytes5(23, 3, 3, clenb[0], clenb[1]);
+        let clen0 = clen as u8;
+        let clen1 = (clen / 256) as u8;
+        let ad = bytes5(23, 3, 3, clen1, clen0);
         let cip = aead_encrypt(&ae, &k, &iv_ctr, &inner_plaintext, &ad)?;
-        let rec = ad.concat(&cip);
+        let rec = ad.concat_owned(cip);
         Ok(rec)
     } else {
         Err(payload_too_long)
@@ -37,19 +38,20 @@ pub fn padlen(b:&Bytes,n:usize) -> usize {
     if n > 0 && b[n-1].declassify() == 0 {1 + padlen(&b,n-1)}
     else {0}
 }
-pub fn decrypt_record_payload(ae:&AEADAlgorithm, kiv:&AEKIV, n:u64, ciphertext: &Bytes) -> Res<(ContentType,Bytes)> {
+pub fn decrypt_record_payload(ae:&AEADAlgorithm, kiv:&AEKIV, n:u64, ciphertext: Bytes) -> Res<(ContentType,Bytes)> {
     let (k,iv) = kiv;
     let iv_ctr = derive_iv_ctr(&ae, &iv, n);
     let clen = ciphertext.len() - 5;
     if clen <= 65536 && clen > 16 {
-        let clenb = u16_to_be_bytes(clen as u16);
-        let ad = bytes5(23, 3, 3, clenb[0], clenb[1]);
+        let clen0 = clen as u8;
+        let clen1 = (clen / 256) as u8;
+        let ad = bytes5(23, 3, 3, clen1, clen0);
         check_eq(&ad,&ciphertext.slice_range(0..5))?;
-        let cip = ciphertext.slice_range(5..ciphertext.len());
+        let (_,cip) = ciphertext.split_off(5);
         let plain = aead_decrypt(&ae, &k, &iv_ctr, &cip, &ad)?;
         let payload_len = plain.len() - padlen(&plain,plain.len()) - 1;
         let ct = get_content_type(plain[payload_len].declassify())?;
-        let payload = plain.slice_range(0..payload_len);       
+        let (payload,_) = plain.split_off(payload_len);       
         Ok((ct,payload))
     } else {
         Err(payload_too_long)
