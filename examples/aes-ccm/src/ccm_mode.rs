231359306a53268354ee7dce62ece00afacbbbf5
Tanmay Garg <40921153+tanmay2004@users.noreply.github.com>
2021-09-07 09:49:32 +0530

    Use usize more

--- examples/aes-ccm/src/ccm_mode.rs
+++ examples/aes-ccm/src/ccm_mode.rs
@@ -4,104 +4,108 @@ use hacspec_aes::*;
 pub type AesCcmResult = Result<ByteSeq, u8>;
 pub const INVALID: u8 = 0u8;
 
-fn format_func(a: &ByteSeq, n: &ByteSeq, p: &ByteSeq, t: u8, alen: u64, nlen: u8, plen: u64) -> ByteSeq {
-    let mut r = 0u64;
-    let mut a_octets = 10u64;
+fn format_func(a: &ByteSeq, n: &ByteSeq, p: &ByteSeq, t: usize, alen: usize, nlen: usize, plen: usize) -> ByteSeq {
+    let mut r = 0;
+    let mut a_octets = 0;
+    let mut flags: u8 = 0u8;
 
-    if alen < 0x800000u64 {
-        a_octets = 2u64;
-    } else {
-        if alen < 0x100000000u64 {
-            a_octets = 6u64;
+    if alen > 0 {
+        flags = 0x40u8;
+
+        if alen < 0x800000 {
+            a_octets = 2;
+        } else {
+            if alen < 0x100000000 {
+                a_octets = 6;
+            } else {
+                a_octets = 10;
+            }
         }
     }
 
-    r = r + ((a_octets+alen+15u64)/16u64)+((plen+15u64)/16u64); // ceiling operation used
-    let mut b = ByteSeq::new((16u64*(r+1u64)) as usize);
+    r = r + ((a_octets+alen+15)/16)+((plen+15)/16); // ceiling operation used
+    let mut b = ByteSeq::new(16 * (r + 1));
 
     // creation of b(0)
-    let qlen: u8 = 15u8 - nlen;
-    let mut flags: u8 = 0u8;
+    let qlen: u8 = 15u8 - (nlen as u8);
 
-    if alen > 0u64 {
-        flags = 0x40u8;
-    }
-
-    flags = flags | (((t-2u8)/2u8) << 3);
+    flags = flags | ((t as u8-2u8)/2u8) << 3;
     flags = flags | (qlen-1u8);
     b[0] = U8(flags);
 
-    for i in 0..(nlen as usize) {
+    for i in 0..nlen {
         b[i+1] = n[i];
     }
 
     let mut plen_copy = plen;
 
     for i in 1..(qlen as usize)+1 {
-        let curr = (plen_copy & 0xFFu64) as u8;
+        let curr = (plen_copy & 0xFF) as u8;
         b[16-i] = U8(curr);
         plen_copy = plen_copy >> 8;
     }
 
     // creation of b(1) to b(u)
-    let x = 0xffu8;
-    let y = 0xfeu8;
+    let mut k: usize = 16; // byte number to set next
 
-    let mut k = 16u64; // byte number to set next
-    let mut alen_copy = alen;
+    if a_octets > 0 {
+        let x = 0xffu8;
+        let y = 0xfeu8;
+        let mut alen_copy = alen;
 
-    if alen >= 0x800000u64 {
-        b[16] = U8(x);
-        k = k + 2u64;
+        if alen >= 0x800000 {
+            b[16] = U8(x);
+            k = k + 2;
 
-        if alen < 0x100000000u64 {
-            b[17] = U8(y);
-        } else {
-            b[17] = U8(x);
+            if alen < 0x100000000 {
+                b[17] = U8(y);
+            } else {
+                b[17] = U8(x);
+            }
         }
-    }
 
-    for i in 1..(a_octets as usize)+1 {
-        let curr = (alen_copy & 0xFFu64) as u8;
-        b[((k+a_octets) as usize)-i] = U8(curr);
-        alen_copy = alen_copy >> 8;
-    }
+        for i in 1..a_octets+1 {
+            let curr = (alen_copy & 0xFF) as u8;
+            b[k + a_octets - i] = U8(curr);
+            alen_copy = alen_copy >> 8;
+        }
 
-    k = k + a_octets;
+        k = k + a_octets;
 
-    for i in 0..(alen as usize) {
-        b[i+(k as usize)] = a[i];
-    }
+        for i in 0..alen {
+            b[i+k]= a[i];
+        }
 
-    k = k + alen;
+        k = k + alen;
 
-    for _t in 0..16 {
-        if k % 16u64 != 0u64 {
-            // add zero padding for Associated Data
-            b[k as usize] = U8(0u8);
-            k = k + 1u64;
+        for _t in 0..16 {
+            if k % 16 != 0 {
+                // add zero padding for Associated Data
+                b[k] = U8(0u8);
+                k = k + 1;
+            }
         }
     }
 
     // creation of b(u+1) to b(r)
-    for i in 0..(plen as usize) {
-        b[i+(k as usize)] = p[i];
+    for i in 0..plen {
+        b[i+k] = p[i];
     }
 
     k = k + plen;
 
     for _t in 0..16 {
-        if k % 16u64 != 0u64 {
+        if k % 16 != 0 {
             // add zero padding for Payload
-            b[k as usize] = U8(0u8);
-            k = k + 1u64;
+            b[k] = U8(0u8);
+            k = k + 1;
         }
     }
 
     b
 }
 
-fn get_t(b: &ByteSeq, key: Key128, num: u8) -> ByteSeq {
+fn get_t(b: &ByteSeq, key: Key128, num: usize) -> ByteSeq {
     let b0 = b.get_exact_chunk(16, 0);
     let bloc = Block::from_seq(&b0);
     let mut y_curr = aes128_encrypt_block(key, bloc);
@@ -112,25 +116,25 @@ fn get_t(b: &ByteSeq, key: Key128, num: u8) -> ByteSeq {
         y_curr = aes128_encrypt_block(key, b_curr);
     }
 
-    ByteSeq::from_seq(&(y_curr.slice(0, num as usize)))
+    ByteSeq::from_seq(&(y_curr.slice(0, num)))
 }
 
-fn counter_func(n: &ByteSeq, nlen: u8, m: u64) -> ByteSeq {
-    let mut ctr = ByteSeq::new((16u64 * (m+1u64)) as usize);
-    let qlen: u8 = 15u8 - nlen;
+fn counter_func(n: &ByteSeq, nlen: usize, m: usize) -> ByteSeq {
+    let mut ctr = ByteSeq::new(16 * (m + 1));
+    let qlen: u8 = 15u8 - (nlen as u8);
     let flag = qlen - 1u8;
 
-    for i in 0..(m as usize)+1 {
+    for i in 0..m+1 {
         let mut icopy = i as u64;
 
         let k = 16 * i;
         ctr[k] = U8(flag);
 
-        for j in 0..(nlen as usize) {
-            ctr[((k+j) as usize)+1] = n[j];
+        for j in 0..nlen {
+            ctr[k+j+1] = n[j];
         }
 
-        for x in 1..16-(nlen as usize)-1 {
+        for x in 1..15-nlen {
             let curr = icopy & 0xFFu64;
             ctr[k+16-x] = U8(curr as u8);
             icopy = icopy >> 8;
@@ -140,13 +144,13 @@ fn counter_func(n: &ByteSeq, nlen: u8, m: u64) -> ByteSeq {
     ctr
 }
 
-fn ctr_cipher(ctr: &ByteSeq, key: Key128, m: u64) -> (ByteSeq, ByteSeq) {
+fn ctr_cipher(ctr: &ByteSeq, key: Key128, m: usize) -> (ByteSeq, ByteSeq) {
     let ctr_zero = Block::from_seq(&ctr.get_exact_chunk(16, 0));
     let key_copy = key.clone();
     let s0 = ByteSeq::from_seq(&aes128_encrypt_block(key, ctr_zero));
-    let mut s = ByteSeq::new((16u64*m) as usize);
+    let mut s = ByteSeq::new(16 * m);
 
-    for i in 1..(m as usize)+1 {
+    for i in 1..m+1 {
         let new_copy = key_copy.clone();
         let ctr_block = Block::from_seq(&ctr.get_exact_chunk(16, i));
         let s_curr = aes128_encrypt_block(new_copy, ctr_block);
@@ -157,41 +161,43 @@ fn ctr_cipher(ctr: &ByteSeq, key: Key128, m: u64) -> (ByteSeq, ByteSeq) {
     (s0, s)
 }
 
-pub fn encrypt_ccm(a: ByteSeq, n: ByteSeq, pay: ByteSeq, key: Key128, tlen: u8, alen: u64, nlen: u8, plen: u64) -> ByteSeq {
+pub fn encrypt_ccm(a: ByteSeq, n: ByteSeq, pay: ByteSeq, key: Key128, tlen: usize) -> ByteSeq {
     let key_copy = key.clone();
+    let plen = pay.len();
+    let alen = a.len();
+    let nlen = n.len();
+
     let b = format_func(&a, &n, &pay, tlen, alen, nlen, plen); // step 1
     let t = get_t(&b, key, tlen); // steps 2 to 4
 
-    let m = (plen+15u64)/16u64; // round up
+    let m = (plen + 15) / 16; // round up
     let counter = counter_func(&n, nlen, m);
     let (s0, s) = ctr_cipher(&counter, key_copy, m);
 
-    let cipherlen = t.len()+pay.len(); let pl = pay.len();
+    let cipherlen = tlen + plen;
     let mut ciphertext = ByteSeq::new(cipherlen);
 
-    let pay_xor = pay ^ s.get_exact_chunk(plen as usize, 0);
-    ciphertext = ciphertext.set_exact_chunk(plen as usize, 0, &pay_xor);
-    let t_xor = t ^ s0.get_exact_chunk(tlen as usize, 0);
+    let pay_xor = pay ^ s.get_exact_chunk(plen, 0);
+    ciphertext = ciphertext.set_exact_chunk(plen, 0, &pay_xor);
+    let t_xor = t ^ s0.get_exact_chunk(tlen, 0);
 
-    for i in pl..cipherlen {
-        ciphertext[i] = t_xor[i-pl];
+    for i in plen..cipherlen {
+        ciphertext[i] = t_xor[i-plen];
     }
 
     ciphertext
 }
 
-pub fn decrypt_ccm(adata: ByteSeq, nonce: ByteSeq, ciph: ByteSeq, clen: u8, key: Key128, tlen: u8, alen: u64, nlen: u8) -> AesCcmResult {
+pub fn decrypt_ccm(adata: ByteSeq, nonce: ByteSeq, key: Key128, ciph: ByteSeq, clen: usize, tlen: usize) -> AesCcmResult {
     if clen > tlen {
-        let m = (clen-tlen+15u8) / 16u8;
-        let counter = counter_func(&nonce, nlen, m as u64);
-        let (s0, s) = ctr_cipher(&counter, key, m as u64);
+        let m = (clen - tlen + 15) / 16;
+        let counter = counter_func(&nonce, nonce.len(), m);
+        let (s0, s) = ctr_cipher(&counter, key, m);
 
-        let x = (clen - tlen) as usize;
-        let p = ciph.get_exact_chunk(x, 0) ^ s.get_exact_chunk(x, 0);
+        let p = ciph.get_exact_chunk(clen - tlen, 0) ^ s.get_exact_chunk(clen - tlen, 0);
 
-        let u_tlen = tlen as usize;
-        let t = ciph.slice_range(ciph.len()-u_tlen..ciph.len()) ^ s0.get_exact_chunk(u_tlen, 0);
-        let b = format_func(&adata, &nonce, &p, tlen, alen, nlen, p.len() as u64);
+        let t = ciph.slice_range(clen-tlen..clen) ^ s0.get_exact_chunk(tlen, 0);
+        let b = format_func(&adata, &nonce, &p, tlen, adata.len(), nonce.len(), p.len());
 
         if get_t(&b, key, tlen) == t {
             AesCcmResult::Ok(p)
