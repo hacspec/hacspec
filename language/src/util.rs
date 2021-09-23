45e6ddb3b7a90932d3de7f7b2ef74819a05747d8
Lasse Letager Hansen <lasse@letager.dk>
2021-09-23 09:15:07 +0200

    Added description of how to use  and

--- language/src/util.rs
+++ language/src/util.rs
@@ -7,6 +7,8 @@ USAGE:
 
 FLAGS:
     -v               Verbosity
+    --init           Creates a '<output>_template' file along with the output
+    --update         Merges changes into output file based on the template file
 
 OPTIONS:
     -o <FILE>        Name of the F* (.fst), Easycrypt (.ec), or Coq (.v) output file
