bfeae0f70e27327ad2da13bf7cbad627f2e413b3
Lasse Letager Hansen <lasse@letager.dk>
2021-10-27 13:02:41 +0200

    Allow let binding patterns for tuple structs for coq backend

--- language/src/rustspec_to_coq.rs
+++ language/src/rustspec_to_coq.rs
@@ -839,38 +839,44 @@ fn translate_expression<'a>(e: Expression, top_ctx: &'a TopLevelContext) -> RcDo
                 .append(RcDoc::space())
                 .append(make_paren(translate_expression(e2, top_ctx)))
         }
-        Expression::NewArray(_array_name, inner_ty, args) => {
-            let inner_ty = inner_ty.unwrap();
-            // inner_ty is the type of the cell elements
-            // TODO: do the case when _array_name is None (the Seq case)
-            match _array_name {
-                // Seq case
-                None => make_list(
-                    args.into_iter()
-                        .map(|(e, _)| translate_expression(e.clone(), top_ctx)),
-                ),
-                Some(_) =>
-                // Array case
-                {
-                    RcDoc::as_string(format!("{}_from_list", ARRAY_MODULE))
-                        .append(RcDoc::space())
-                        .append(translate_base_typ(inner_ty.clone()))
-                        .append(RcDoc::space())
-                        .append(make_paren(
-                            make_let_binding(
-                                RcDoc::as_string("l"),
-                                None,
-                                make_list(
-                                    args.into_iter()
-                                        .map(|(e, _)| translate_expression(e.clone(), top_ctx)),
-                                ),
-                                false,
-                            )
-                            .append(RcDoc::space())
-                            .append(RcDoc::as_string("l")),
-                        ))
+        Expression::NewArray(_, typ, args) => {
+            // retrieves the element type of the array. For named types (aliases), it looks up
+            // the original definition in the typ_dict.
+            let inner_ty = match typ {
+                Some(BaseTyp::Named(ident, _)) => {
+                    let ident = &ident.0;
+                    match top_ctx.typ_dict.get(ident) {
+                        Some((alias_typ, DictEntry::Array)) => {
+                            match (alias_typ.1).0.clone() {
+                                BaseTyp::Array(_size, inner_ty) => inner_ty.as_ref().clone().0,
+                                _ => panic!(),
+                            }
+                            // translate_prefix_for_func_name((alias_typ.1).0.clone(), top_ctx)
+                        }
+                        _ => panic!(),
+                    }
                 }
-            }
+                Some(BaseTyp::Array(_size, inner_ty)) => inner_ty.as_ref().clone().0,
+                // Should not happen - array should always have an element type
+                _ => panic!(),
+            };
+            RcDoc::as_string(format!("{}_from_list", ARRAY_MODULE))
+                .append(RcDoc::space())
+                .append(translate_base_typ(inner_ty.clone()))
+                .append(RcDoc::space())
+                .append(make_paren(
+                    make_let_binding(
+                        RcDoc::as_string("l"),
+                        None,
+                        make_list(
+                            args.into_iter()
+                                .map(|(e, _)| translate_expression(e, top_ctx)),
+                        ),
+                        false,
+                    )
+                    .append(RcDoc::space())
+                    .append(RcDoc::as_string("l")),
+                ))
         }
         Expression::IntegerCasting(x, new_t, old_t) => {
             let old_t = old_t.unwrap();
@@ -1010,7 +1016,11 @@ fn translate_statements<'a>(
                 unimplemented!()
             } else {
                 make_let_binding(
-                    translate_pattern_tick(pat.clone()),
+                    match pat.clone() {
+                        Pattern::SingleCaseEnum(_, _) => RcDoc::as_string("'"),
+                        _ => RcDoc::nil(),
+                    }
+                    .append(translate_pattern_tick(pat.clone())),
                     typ.map(|(typ, _)| translate_typ(typ)),
                     translate_expression(expr.clone(), top_ctx),
                     false,
