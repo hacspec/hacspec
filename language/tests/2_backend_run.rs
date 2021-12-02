bffc146f58f216d8e561f6dd9df184e7617c9ded
Lasse Letager Hansen <lasse@letager.dk>
2021-12-02 09:10:04 +0100

    Exporting makefile

--- language/tests/2_backend_run.rs
+++ language/tests/2_backend_run.rs
@@ -2,6 +2,15 @@ use std::{env, process::Command};
 
 #[test]
 fn run_coq_backend() {
+    let mut cmd = Command::new("coq_makefile");
+    cmd.args(&["-f","_CoqProject"]);
+    cmd.args(&["-o","makefile"]);
+    cmd.current_dir("./tests/coq/");
+    cmd.envs(env::vars());
+    let status = cmd.status();
+    let status = status.expect("Error creating coq makefile");
+    assert!(status.success());
+
     let mut cmd = Command::new("make");
     cmd.current_dir("./tests/coq/");
     cmd.envs(env::vars());
