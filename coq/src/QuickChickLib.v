28231db04a0b52c942e4691a56a5497fd60f53f8
Lasse Letager Hansen <lasse@letager.dk>
2021-12-02 18:06:36 +0100

    Generalized some instances of G and Gen

--- coq/src/QuickChickLib.v
+++ coq/src/QuickChickLib.v
@@ -35,36 +35,54 @@ Definition g_int64 : G (int64) :=
 Definition g_uint_size : G (uint_size) := arbitrary.
 #[global] Instance gen_uint_size : Gen (uint_size) := Build_Gen uint_size g_uint_size.
 
+
+Inductive dependent_pair {A} (n : nat) :=
+| elem :  forall (l : list A), length l = n -> dependent_pair n.
+
+Fixpoint g_listOf_aux {A} (gen : G (A)) (n : nat) : G (@dependent_pair A n).
+  destruct n.
+  - apply (returnGen).
+    exists [].
+    easy.
+  - apply (bindGen gen).
+    intros a.
+    apply (bindGen (g_listOf_aux A gen n)).
+    intros l.
+    apply returnGen.
+    destruct l as [l H].
+    exists (a :: l).
+    cbn.
+    rewrite H.
+    easy.
+Defined.
+
+Definition g_listOf {A} (gen : G (A)) : G (list A) :=
+  bindGen arbitrary (fun n : nat => bindGen (g_listOf_aux gen n) (fun '(elem _ l _) => returnGen l)).
+
 #[global] Instance show_nseq {A} `{Show A} n : Show (nseq A n) :=
   Build_Show (nseq A n) (fun x =>
      match x with
      | Vector.nil _ => "[]"%string
      | Vector.cons _ x n xs => ("[" ++ fold_left (fun a b => (a ++ " " ++ show b)) xs (show x) ++ "]")%string
      end).
-
-Theorem unsized_g_int8 : Unsized g_int8.
+ 
+Definition array_from_list_ (A : Type) (n : nat) (l : list A) `{n = (Datatypes.length l)} : nseq A n.
 Proof.
-  unfold g_int8.
-  apply bindUnsized.
-  apply chooseUnsized.
-  intros.
-  apply unsizedReturn.
-Qed.
-
-Definition g_nseq {A} `{H : Gen A} `{@Unsized A (@arbitrary A H)} n : G (nseq A n) :=
-  (@bindGen' (list A) (nseq A n) (vectorOf n (arbitrary : G A))) (fun l sem =>
-  returnGen (@eq_rect_r (nat) (Datatypes.length l) (nseq A) (array_from_list A l) n (Logic.eq_sym (proj1 ( proj1 (@semVectorOfUnsized A (arbitrary : G A) n _ l) sem ))))).
-
-Definition g_nseq' n : G (nseq (int8) (usize n)). (* (usize *)
-  intros.
-  apply (@bindGen' (list int8) (nseq int8 (usize n)) (vectorOf (usize n) g_int8)).
-  intros l sem.
-  apply returnGen.
-  rewrite <- (proj1 ( proj1 (@semVectorOfUnsized int8 g_int8 (usize n) unsized_g_int8 l) sem )).
-  apply (array_from_list int8 l).
+  rewrite H.
+  apply array_from_list.
 Defined.
+Definition g_nseq {A} `{Gen A} n : G (nseq A n). (* (usize *)
+   intros.
+   apply (bindGen' (g_listOf_aux (arbitrary : G A) n)).
+   intros l sem.
+   apply returnGen.
 
-#[global] Instance gen_nseq {A} `{Gen A} `{@Unsized A (@arbitrary A H)} n : Gen (nseq A n) := Build_Gen (nseq A n) (g_nseq n).
+  destruct l.
+  apply array_from_list_ with (l := l).
+  easy.
+Defined.
+ 
+#[global] Instance gen_nseq {A} `{Gen A} n : Gen (nseq A n) := Build_Gen (nseq A n) (g_nseq  n).
 
 #[global] Instance show_public_byte_seq : Show (public_byte_seq) :=
   Build_Show (public_byte_seq) (fun x =>
@@ -81,6 +99,3 @@ Definition g_public_byte_seq : G (public_byte_seq) :=
   (* listOf (g_int8). (*arbitrary*) *)
 #[global] Instance gen_public_byte_seq : Gen (public_byte_seq) :=
   Build_Gen public_byte_seq g_public_byte_seq.
-
-Check (@g_nseq int8 gen_int8 unsized_g_int8 1).
-Sample (@g_nseq int8 gen_int8 unsized_g_int8 1).
