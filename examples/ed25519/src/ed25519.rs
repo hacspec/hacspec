24b38ad29635d1c43e0c8ccdd3d978fcc7868c67
Kasper S. Nielsen (laptop) <Kasper@s-nielsen.dk>
2022-01-25 13:14:33 +0100

    ed25519 + sha512

--- examples/ed25519/src/ed25519.rs
+++ examples/ed25519/src/ed25519.rs
@@ -1,8 +1,5 @@
 use hacspec_lib::*;
-use hacspec_attributes::*;
-
-
-bytes!(Sha512Digest, 512 / 8);
+use hacspec_sha512::*;
 
 public_nat_mod!(
     type_name: Ed25519FieldElement,
@@ -23,45 +20,104 @@ public_nat_mod!(
     modulo_value: "1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed"
 );
 
-type EdPoint = (Ed25519FieldElement, Ed25519FieldElement, Ed25519FieldElement, Ed25519FieldElement);
+type EdPoint = (
+    Ed25519FieldElement,
+    Ed25519FieldElement,
+    Ed25519FieldElement,
+    Ed25519FieldElement,
+);
 
 bytes!(CompressedEdPoint, 32);
 bytes!(SerializedScalar, 32);
 bytes!(Signature, 64);
 
-const BASE: CompressedEdPoint = CompressedEdPoint(secret_array!(U8, 
-    [0x58u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8,
-    0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8,
-    0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8,
-    0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8 ]));
+#[derive(Debug)]
+pub enum Error {
+    InvalidPublickey,
+    InvalidSignature,
+}
 
-fn is_negative(x : Ed25519FieldElement) -> U8 {
-    if x.bit(255) {
-        U8(1u8) 
-    }
-    else {
+pub type VerifyResult = Result<(), Error>;
+
+#[rustfmt::skip]
+const BASE: CompressedEdPoint = CompressedEdPoint(secret_array!(
+    U8, 
+    [
+        0x58u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8,
+        0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8,
+        0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8,
+        0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8, 0x66u8 
+    ]
+));
+
+#[rustfmt::skip]
+const CONSTANT_L: SerializedScalar = SerializedScalar(secret_array!(
+    U8, 
+    [
+        0xedu8, 0xd3u8, 0xf5u8, 0x5cu8, 0x1au8, 0x63u8, 0x12u8, 0x58u8,
+        0xd6u8, 0x9cu8, 0xf7u8, 0xa2u8, 0xdeu8, 0xf9u8, 0xdeu8, 0x14u8,
+        0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 
+        0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x10u8 
+    ]
+));
+
+#[rustfmt::skip]
+const CONSTANT_P3_8: SerializedScalar = SerializedScalar(secret_array!(
+    U8, 
+    [
+        0xfeu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 
+        0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 
+        0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 
+        0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0x0fu8 
+    ]
+));
+
+#[rustfmt::skip]
+const CONSTANT_P1_4: SerializedScalar = SerializedScalar(secret_array!(
+    U8, 
+    [
+        0xfbu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 
+        0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 
+        0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 
+        0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0xffu8, 0x1fu8 
+    ]
+));
+
+#[rustfmt::skip]
+const CONSTANT_D: SerializedScalar = SerializedScalar(secret_array!(
+    U8, 
+    [
+        0xa3u8, 0x78u8, 0x59u8, 0x13u8, 0xcau8, 0x4du8, 0xebu8, 0x75u8, 
+        0xabu8, 0xd8u8, 0x41u8, 0x41u8, 0x4du8, 0x0au8, 0x70u8, 0x00u8, 
+        0x98u8, 0xe8u8, 0x79u8, 0x77u8, 0x79u8, 0x40u8, 0xc7u8, 0x8cu8, 
+        0x73u8, 0xfeu8, 0x6fu8, 0x2bu8, 0xeeu8, 0x6cu8, 0x03u8, 0x52u8 
+    ]
+));
+
+fn is_negative(x: Ed25519FieldElement) -> U8 {
+    if x.bit(0) {
+        U8(1u8)
+    } else {
         U8(0u8)
     }
 }
 
-pub fn compress(p : EdPoint) -> CompressedEdPoint {
+fn compress(p: EdPoint) -> CompressedEdPoint {
     let (x, y, z, _) = p;
     let z_inv = z.inv();
     let x = x * z_inv;
     let y = y * z_inv;
-    let mut s : ByteSeq = y.to_byte_seq_le();
+    let mut s: ByteSeq = y.to_byte_seq_le();
     s[31usize] = s[31usize] ^ (is_negative(x) << 7); // setting top bit
     CompressedEdPoint::from_slice(&s, 0, 32)
 }
 
-pub fn sqrt(a : Ed25519FieldElement) -> Option<Ed25519FieldElement> {
-    //constants -- should be moved to constants
-    let p3_8 = (Ed25519FieldElement::ZERO() - Ed25519FieldElement::from_literal(5u128)) / Ed25519FieldElement::from_literal(8u128) + Ed25519FieldElement::ONE();
-    let p1_4 = (Ed25519FieldElement::ZERO() - Ed25519FieldElement::ONE()) / Ed25519FieldElement::from_literal(4u128);
-    
+fn sqrt(a: Ed25519FieldElement) -> Option<Ed25519FieldElement> {
+    let p3_8 = Ed25519FieldElement::from_byte_seq_le(CONSTANT_P3_8);
+    let p1_4 = Ed25519FieldElement::from_byte_seq_le(CONSTANT_P1_4);
+
     let x_c = a.pow_self(p3_8);
-    //println!("{}", x_c);
-    let mut result : Option<Ed25519FieldElement> = Option::<Ed25519FieldElement>::None;
+    let mut result: Option<Ed25519FieldElement> = Option::<Ed25519FieldElement>::None;
     if x_c * x_c == a {
         result = Some(x_c);
     };
@@ -72,39 +128,29 @@ pub fn sqrt(a : Ed25519FieldElement) -> Option<Ed25519FieldElement> {
     result
 }
 
-fn correct_sign(x :Ed25519FieldElement, y: Ed25519FieldElement, z: Ed25519FieldElement, x_s: U8) -> EdPoint {
-    let x_r = is_negative(x);
-    let mut x = x;
-    if x_r.declassify() != x_s.declassify() { // Could probably do without declassifying
-        x = Ed25519FieldElement::ZERO() - x;
-    }
-    (x, y, z, x * y)
-}
+fn decompress(p: CompressedEdPoint) -> Option<EdPoint> {
+    let d = Ed25519FieldElement::from_byte_seq_le(CONSTANT_D);
 
-pub fn decompress(p: CompressedEdPoint) -> Option<EdPoint> {
-    //constant -- should be moved to constants
-    let d = Ed25519FieldElement::ZERO() - (Ed25519FieldElement::from_literal(121665u128) * Ed25519FieldElement::from_literal(121666u128).inv() );
-
-    let x_s = p[31usize] & U8(128u8) >> 7;
+    let x_s = (p[31usize] & U8(128u8)) >> 7;
     let mut y_s = p;
     y_s[31usize] = y_s[31usize] & U8(127u8);
-    let y = Ed25519FieldElement::from_byte_seq_le(y_s); 
+    let y = Ed25519FieldElement::from_byte_seq_le(y_s);
     let z = Ed25519FieldElement::ONE();
     let yy = y * y;
     let u = yy - z;
     let v = d * yy + z;
     let xx = u * v.inv();
-    let x_o = sqrt(xx);
-    match x_o {
-        Option::<Ed25519FieldElement>::None => Option::<EdPoint>::None,
-        Option::<Ed25519FieldElement>::Some(x) => Some(correct_sign(x, y, z, x_s))
+    let x_o = sqrt(xx)?;
+    let x_r = is_negative(x_o);
+    let mut x = x_o;
+    if x_r.declassify() != x_s.declassify() {
+        x = Ed25519FieldElement::ZERO() - x;
     }
-    
+    Some((x, y, z, x * y))
 }
 
-pub fn point_add(p: EdPoint, q: EdPoint) -> EdPoint {
-    //constant -- should be moved to constants
-    let d_c = Ed25519FieldElement::ZERO() - (Ed25519FieldElement::from_literal(121665u128) * Ed25519FieldElement::from_literal(121666u128).inv() );
+fn point_add(p: EdPoint, q: EdPoint) -> EdPoint {
+    let d_c = Ed25519FieldElement::from_byte_seq_le(CONSTANT_D);
     let two = Ed25519FieldElement::TWO();
 
     let (x1, y1, z1, t1) = p;
@@ -124,9 +170,14 @@ pub fn point_add(p: EdPoint, q: EdPoint) -> EdPoint {
     (x3, y3, z3, t3)
 }
 
-pub fn point_mul(s: Scalar, p: EdPoint) -> EdPoint {
+fn point_mul(s: Scalar, p: EdPoint) -> EdPoint {
     let mut p = p;
-    let mut q = (Ed25519FieldElement::ZERO(), Ed25519FieldElement::ONE(), Ed25519FieldElement::ONE(), Ed25519FieldElement::ZERO());
+    let mut q = (
+        Ed25519FieldElement::ZERO(),
+        Ed25519FieldElement::ONE(),
+        Ed25519FieldElement::ONE(),
+        Ed25519FieldElement::ZERO(),
+    );
     for i in 0..256 {
         if s.bit(i) {
             q = point_add(q, p);
@@ -136,7 +187,24 @@ pub fn point_mul(s: Scalar, p: EdPoint) -> EdPoint {
     q
 }
 
-pub fn point_eq(p: EdPoint, q: EdPoint) -> bool {
+fn point_mul_by_cofactor(p: EdPoint) -> EdPoint {
+    let p2 = point_add(p, p);
+    let p4 = point_add(p2, p2);
+    let p8 = point_add(p4, p4);
+    p8
+}
+
+fn point_neg(p: EdPoint) -> EdPoint {
+    let (x, y, z, t) = p;
+    (
+        Ed25519FieldElement::ZERO() - x,
+        y,
+        z,
+        Ed25519FieldElement::ZERO() - t,
+    )
+}
+
+fn point_eq(p: EdPoint, q: EdPoint) -> bool {
     let (x1, y1, z1, _) = p;
     let (x2, y2, z2, _) = q;
     x1 * z2 == x2 * z1 && y1 * z2 == y2 * z1
@@ -160,11 +228,11 @@ pub fn secret_to_public(sk: SerializedScalar) -> CompressedEdPoint {
     compress(a)
 }
 
-pub fn sc_modl_to_sc(s: ScalarModL) -> Scalar {
+fn sc_modl_to_sc(s: ScalarModL) -> Scalar {
     Scalar::from_byte_seq_le(s.to_byte_seq_le().slice(0, 32))
 }
 
-pub fn sc_to_sc_modl(s: Scalar) -> ScalarModL {
+fn sc_to_sc_modl(s: Scalar) -> ScalarModL {
     ScalarModL::from_byte_seq_le(s.to_byte_seq_le().concat(&ByteSeq::new(32)))
 }
 
@@ -184,12 +252,154 @@ pub fn sign(sk: SerializedScalar, msg: &ByteSeq) -> Signature {
     Signature::new().update(0, &r_s).update(32, &s_bytes)
 }
 
-#[hacspec_unsafe]
-pub fn sha512(msg: &ByteSeq) -> Sha512Digest {
-    use ed25519_dalek::{Sha512, Digest};
+pub fn verify(pk: CompressedEdPoint, signature: Signature, msg: &ByteSeq) -> VerifyResult {
+    let b = decompress(BASE).unwrap();
+    let a = decompress(pk).ok_or(Error::InvalidPublickey)?;
+    let r_bytes = CompressedEdPoint::from_slice(&signature, 0, 32);
+    let r = decompress(r_bytes).ok_or(Error::InvalidSignature)?;
+    let s = Scalar::from_byte_seq_le(signature.slice(32, 32));
+    let l = Scalar::from_byte_seq_le(CONSTANT_L);
+    if s >= l {
+        VerifyResult::Err(Error::InvalidSignature)?;
+    }
+    let k = sha512(&r_bytes.concat(&pk).concat(msg));
+    let h = sc_modl_to_sc(ScalarModL::from_byte_seq_le(k));
 
-    let mut h: Sha512 = Sha512::new();
-    h.update(msg.to_native());
-    let r = h.finalize();
-    Sha512Digest::from_public_slice(r.as_slice())
+    let r_prime = point_add(point_mul(s, b), point_neg(point_mul(h, a))); // R' = [s]B - [c]A
+    let check = point_mul_by_cofactor(point_add(r, point_neg(r_prime)));
+    let identity = (
+        Ed25519FieldElement::ZERO(),
+        Ed25519FieldElement::ONE(),
+        Ed25519FieldElement::ONE(),
+        Ed25519FieldElement::ZERO(),
+    );
+
+    if point_eq(check, identity) {
+        VerifyResult::Ok(())
+    } else {
+        VerifyResult::Err(Error::InvalidSignature)
+    }
+}
+
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_compress_decompress() {
+        let sk = SerializedScalar::from_hex(
+            "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60",
+        );
+        let msg = &ByteSeq::from_public_slice(b"");
+        let (_, prefix) = secret_expand(sk);
+        //let a = Scalar::from_byte_seq_le(a);
+        let b = decompress(BASE).unwrap();
+        //let a_p = compress(point_mul(a, b));
+        let r_h = sha512(&prefix.concat(msg));
+        let r = ScalarModL::from_byte_seq_le(r_h);
+        let r_p = point_mul(sc_modl_to_sc(r), b);
+        let r_s = compress(r_p);
+        assert!(point_eq(r_p, decompress(r_s).unwrap()));
+    }
+
+    #[test]
+    fn test_secret_to_public() {
+        let s = SerializedScalar::from_hex(
+            "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60",
+        );
+        let result = CompressedEdPoint::from_hex(
+            "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a",
+        );
+        let a = secret_to_public(s);
+        assert_bytes_eq!(a, result);
+
+        let s = SerializedScalar::from_hex(
+            "4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb",
+        );
+        let result = CompressedEdPoint::from_hex(
+            "3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c",
+        );
+        let a = secret_to_public(s);
+        assert_bytes_eq!(a, result);
+    }
+
+    #[test]
+    fn test_sign() {
+        let s = SerializedScalar::from_hex(
+            "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60",
+        );
+        let msg = ByteSeq::from_public_slice(b"");
+        let sig = sign(s, &msg);
+        let result = Signature::from_hex(
+            "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e06522490155\
+            5fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b",
+        );
+        assert_bytes_eq!(sig, result);
+
+        let s = SerializedScalar::from_hex(
+            "4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb",
+        );
+        let msg = ByteSeq::from_hex("72");
+        let sig = sign(s, &msg);
+        let result = Signature::from_hex(
+            "92a009a9f0d4cab8720e820b5f642540a2b27b5416503f8fb3762223ebdb69da\
+            085ac1e43e15996e458f3613d0f11d8c387b2eaeb4302aeeb00d291612bb0c00",
+        );
+        assert_bytes_eq!(sig, result);
+
+        let s = SerializedScalar::from_hex(
+            "c5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7",
+        );
+        let msg = ByteSeq::from_hex("af82");
+        let sig = sign(s, &msg);
+        let result = Signature::from_hex(
+            "6291d657deec24024827e69c3abe01a30ce548a284743a445e3680d7db5ac3ac\
+            18ff9b538d16f290ae67f760984dc6594a7c15e9716ed28dc027beceea1ec40a",
+        );
+        assert_bytes_eq!(sig, result);
+    }
+
+    #[test]
+    fn test_verify() {
+        let a = CompressedEdPoint::from_hex(
+            "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a",
+        );
+        let msg = ByteSeq::from_hex("");
+        let sig = Signature::from_hex(
+            "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e06522490155\
+            5fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b",
+        );
+        let result = verify(a, sig, &msg);
+        assert!(result.is_ok());
+
+        let s = SerializedScalar::from_hex(
+            "c5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7",
+        );
+        let a = secret_to_public(s);
+        let msg = ByteSeq::from_hex("af82");
+        let sig = sign(s, &msg);
+        let sig_result = Signature::from_hex(
+            "6291d657deec24024827e69c3abe01a30ce548a284743a445e3680d7db5ac3ac\
+            18ff9b538d16f290ae67f760984dc6594a7c15e9716ed28dc027beceea1ec40a",
+        );
+        assert_bytes_eq!(sig, sig_result);
+        assert!(verify(a, sig, &msg).is_ok());
+    }
+
+    #[quickcheck]
+    fn test_sign_verify(sk: (u128, u128), msg: String) -> bool {
+        let (sk1, sk2) = sk;
+        let sk = [sk2.to_le_bytes(), sk1.to_le_bytes()].concat();
+        let sk = SerializedScalar::from_public_slice(&sk);
+        let pk = secret_to_public(sk);
+        let msg = &ByteSeq::from_public_slice(msg.as_bytes());
+        let signature = sign(sk, &msg);
+        verify(pk, signature, msg).is_ok()
+    }
 }
