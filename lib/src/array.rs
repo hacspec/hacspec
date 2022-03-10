f8fa4cb80c757b907932ea87e4b6fe5596d79f10
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-03-10 12:31:52 +0100

    some functions for public sequences

--- lib/src/array.rs
+++ lib/src/array.rs
@@ -665,6 +665,33 @@ macro_rules! _public_array {
                 self.0[..] == other.0[..]
             }
         }
+        impl $name {
+            #[cfg_attr(feature = "use_attributes", unsafe_hacspec)]
+            pub fn to_be_bytes(&self) -> Seq<u8> {
+                const FACTOR: usize = core::mem::size_of::<$t>();
+                let mut out: Seq<u8> = Seq::new($l * FACTOR);
+                for i in 0..$l {
+                    let tmp = <$t>::to_be_bytes(self[i]);
+                    for j in 0..FACTOR {
+                        out[i * FACTOR + j] = tmp[j];
+                    }
+                }
+                out
+            }
+
+            #[cfg_attr(feature = "use_attributes", unsafe_hacspec($name))]
+            pub fn to_le_bytes(&self) -> Seq<u8> {
+                const FACTOR: usize = core::mem::size_of::<$t>();
+                let mut out: Seq<u8> = Seq::new($l * FACTOR);
+                for i in 0..$l {
+                    let tmp = <$t>::to_le_bytes( self[i]);
+                    for j in 0..FACTOR {
+                        out[i * FACTOR + j] = tmp[j];
+                    }
+                }
+                out
+            }
+        }
     };
 }
 
