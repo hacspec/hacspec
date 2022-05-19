c7044a198c88d95d1645899e94e5e381eca659d2
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2022-05-19 12:09:11 +0000

    deploy: b1a4d8564e0f95209703672dc09749bc3c125fc4

--- hacspec_ecdsa_p256_sha256/sidebar-items.js
+++ hacspec_ecdsa_p256_sha256/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error",""]],"fn":[["ecdsa_p256_sha256_sign",""],["ecdsa_p256_sha256_verify",""],["sign",""],["verify",""]],"type":[["PublicKey",""],["SecretKey",""],["Signature",""],["SignatureResult",""],["VerifyResult",""]]});
+initSidebarItems({"enum":[["Error",""]],"fn":[["ecdsa_p256_sha256_sign",""],["ecdsa_p256_sha256_verify",""]],"type":[["P256PublicKey",""],["P256SecretKey",""],["P256Signature",""],["P256SignatureResult",""],["P256VerifyResult",""]]});
