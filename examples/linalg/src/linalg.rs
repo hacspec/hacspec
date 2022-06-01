9d299553d66aa63adca5c6787d1a4b3c1bd425b8
rasmus-kirk <mail@rasmuskirk.com>
2022-06-01 11:39:29 +0200

    Added errors, documented comments, +default member

--- examples/linalg/src/linalg.rs
+++ examples/linalg/src/linalg.rs
@@ -24,19 +24,27 @@ pub type DimType = usize;
 pub type Scalar = i128;
 pub type Dims = (DimType, DimType);
 pub type Matrix = (Dims, Seq<Scalar>);
-pub type MatRes = Result<Matrix, ()>;
-pub type ScalRes = Result<Scalar, ()>;
+pub type MatRes = Result<Matrix, u8>;
+pub type ScalRes = Result<Scalar, u8>;
 
-// Generate new matrix using rows, cols and a seq.
+// === Errors === //
+
+const ROW_COL_MISMATCH: u8 = 10;
+const INDEX_OUT_OUT_OF_BOUNDS: u8 = 11;
+const SLICE_OUT_OUT_OF_BOUNDS: u8 = 11;
+
+// === External Functions === //
+
+/// Generate new matrix using rows, cols and a seq.
 pub fn new(rows: DimType, cols: DimType, seq: Seq<Scalar>) -> MatRes {
     if seq.len() > 0 && rows * cols == seq.len() {
         MatRes::Ok(((rows, cols), seq))
     } else {
-        MatRes::Err(())
+        MatRes::Err(ROW_COL_MISMATCH)
     }
 }
 
-// Generate a n*m matrix filled with a given scalar
+/// Generate a n*m matrix filled with a given scalar
 pub fn repeat(n: DimType, m: DimType, scalar: Scalar) -> MatRes {
     let mut ret = Seq::<Scalar>::new(n * m);
 
@@ -47,19 +55,19 @@ pub fn repeat(n: DimType, m: DimType, scalar: Scalar) -> MatRes {
     new(n, m, ret)
 }
 
-// Generate a n*m matrix filled with zeros.
+/// Generate a n*m matrix filled with zeros.
 pub fn zeros(n: DimType, m: DimType) -> MatRes {
     repeat(n, m, Scalar::ZERO())
 }
 
-// Generate a n*m matrix filled with ones.
+/// Generate a n*m matrix filled with ones.
 pub fn ones(n: DimType, m: DimType) -> MatRes {
     repeat(n, m, Scalar::ONE())
 }
 
-// Generates an identity matrix. If the matrix is not square,
-// the largest square submatrix (starting at the first row and column)
-// is set to the identity while all other entries are set to zero.
+/// Generates an identity matrix. If the matrix is not square,
+/// the largest square submatrix (starting at the first row and column)
+/// is set to the identity while all other entries are set to zero.
 pub fn identity(n: DimType, m: DimType) -> MatRes {
     let mut ret = Seq::<Scalar>::new(n * m);
 
@@ -71,20 +79,20 @@ pub fn identity(n: DimType, m: DimType) -> MatRes {
     new(n, m, ret)
 }
 
-// Gets the index of a matrix
+/// Gets the index of a matrix
 pub fn index(m: Matrix, i: DimType, j: DimType) -> ScalRes {
     let (dim, seq) = m;
     let (rows, cols) = dim;
     let index = i * cols + j;
 
     if index >= rows * cols {
-        ScalRes::Err(())
+        ScalRes::Err(INDEX_OUT_OUT_OF_BOUNDS)
     } else {
         ScalRes::Ok(seq[index])
     }
 }
 
-// Transposes a matrix
+/// Transposes a matrix
 pub fn transpose(matrix: Matrix) -> Matrix {
     let (dim, seq) = matrix;
     let (rows, cols) = dim;
@@ -101,9 +109,9 @@ pub fn transpose(matrix: Matrix) -> Matrix {
     ((cols, rows), ret)
 }
 
-// Returns a matrix slice, given a Matrix and two Dims (pairs of usize),
-// first Dims pair representing the starting point and second Dims
-// pair the dimensions
+/// Returns a matrix slice, given a Matrix and two Dims (pairs of usize),
+/// first Dims pair representing the starting point and second Dims
+/// pair the dimensions
 pub fn slice(matrix: Matrix, start: Dims, len: Dims) -> MatRes {
     let (dim, seq) = matrix;
     let (rows, cols) = dim;
@@ -111,7 +119,7 @@ pub fn slice(matrix: Matrix, start: Dims, len: Dims) -> MatRes {
     let (len_rows, len_cols) = len;
     let start_index = start_row * cols + start_col;
     let mut ret = Seq::<Scalar>::new(len_rows * len_cols);
-    let mut res = MatRes::Err(());
+    let mut res = MatRes::Err(SLICE_OUT_OF_BOUNDS);
 
     if start_index + len_rows * len_cols <= rows * cols {
         for i in 0..len_rows {
@@ -128,7 +136,7 @@ pub fn slice(matrix: Matrix, start: Dims, len: Dims) -> MatRes {
     res
 }
 
-// Scale a matrix with a given scalar
+/// Scale a matrix with a given scalar
 pub fn scale(matrix: Matrix, scalar: Scalar) -> Matrix {
     let (dim, seq) = matrix;
     let mut ret = Seq::<Scalar>::new(seq.len());
