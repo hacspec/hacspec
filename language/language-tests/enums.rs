9b6f2df5063c7973921bcc8fd099286900de7b94
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-10-06 17:30:33 +0200

    more examples of nested pattern matching

--- language/language-tests/enums.rs
+++ language/language-tests/enums.rs
@@ -1,7 +1,12 @@
 use hacspec_lib::*;
 
-pub enum Foo {
+pub enum Baz {
     CaseX,
+    CaseY(Result<u8, U8>),
+}
+
+pub enum Foo {
+    CaseX(Baz),
     CaseY(u8, Seq<u32>),
 }
 
@@ -11,12 +16,15 @@ pub fn baz(x: Foo) -> Bar {
     let z: Bar = Bar(0u32);
     let Bar(z) = z;
     let y = match x {
-        Foo::CaseX => Foo::CaseY(z as u8, Seq::<u32>::new(1)),
+        Foo::CaseX(Baz::CaseY(Result::<u8, U8>::Ok(x))) => {
+            Foo::CaseY(z as u8 + x, Seq::<u32>::new(1))
+        }
+        Foo::CaseX(_) => Foo::CaseX(Baz::CaseX),
         Foo::CaseY(0u8, b) => Foo::CaseY(1u8, b),
-        Foo::CaseY(_,_) => Foo::CaseY(1u8, Seq::<u32>::new(1)),
+        Foo::CaseY(_, _) => Foo::CaseY(1u8, Seq::<u32>::new(1)),
     };
     match y {
-        Foo::CaseX => Bar(0u32),
+        Foo::CaseX(_) => Bar(0u32),
         Foo::CaseY(a, _b) => Bar(a as u32),
     }
 }
@@ -24,5 +32,5 @@ pub fn baz(x: Foo) -> Bar {
 pub fn baz_im(x: Foo) {
     let z: Bar = Bar(0u32);
     let Bar(z) = z;
-    let _a = z as u8;
+    let a = z as u8;
 }
