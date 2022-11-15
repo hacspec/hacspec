df6b43eb5b4f39021379b869a088898d66c1a54c
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-11-15 16:12:13 +0100

    Add example of unannotated `Some` and `None`

--- language/language-tests/option.rs
+++ language/language-tests/option.rs
@@ -8,3 +8,12 @@ pub fn foo(x: Option<Option<U32>>) -> U32 {
         Option::<Option<U32>>::Some(x) => x.unwrap(),
     }
 }
+
+pub fn foo_unannonated(x: Option<Option<U32>>) -> Option<U32> {
+    let _y: Option<U32> = None;
+    match x {
+        None => Some(U32(0u32)),
+        Some(None) => Some(U32(1u32)),
+        Some(Some(x)) => Some(x),
+    }
+}
