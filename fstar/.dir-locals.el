bbbc4c42427790c3729eeaf0bb0304194e212a7d
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-12-06 21:23:43 +0100

    Add `_vc` folder and reformat Chacha20 & Poly1305

---
+++ fstar/.dir-locals.el
@@ -0,0 +1,6 @@
+((fstar-mode
+  (fstar-subp-prover-args . (
+                             "--include" "/home/lucas/Desktop/hacspec/hacl-star/lib"
+                             "--include" "/home/lucas/Desktop/hacspec/hacl-star/specs"
+                             "--include" "/home/lucas/Desktop/hacspec/fstar"
+                             ))))
