24beece6fa95e7731c12f285eed8d9aac85b2bf5
Benjamin S. Hvass <bshvass@gmail.com>
2023-01-27 08:25:05 +0100

    change xor example

--- examples/xor/src/xor.rs
+++ examples/xor/src/xor.rs
@@ -1,12 +1,5 @@
 use hacspec_lib::*;
 
-fn xor(x_inp : u64, y_inp : u64) -> u64 {
-    let mut x : u64 = x_inp;
-    let mut y : u64 = y_inp;
-    let v = x;
-    let mut r : u64 = v;
-    let v1 = r;
-    let v2 = y;
-    r = v1 ^ v2;
-    r
+fn xor(x : u64, y : u64) -> u64 {
+    x ^ y
 }
