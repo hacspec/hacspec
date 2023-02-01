9c47f06215dc123cfb5839ea41ab33b1b339e0c2
Lasse Letager Hansen <lasse@letager.dk>
2023-02-01 12:54:53 +0100

    Updated xor to fit jasmin

--- coq_ssprove/src/Hacspec_Xor.v
+++ coq_ssprove/src/Hacspec_Xor.v
@@ -23,11 +23,11 @@ Open Scope hacspec_scope.
 Obligation Tactic := try timeout 8 solve_ssprove_obligations.
 
 
-Definition x_0_loc : ChoiceEqualityLocation :=
-  (int64 ; 3%nat).
 Definition y_1_loc : ChoiceEqualityLocation :=
-  (int64 ; 4%nat).
+  (int64 ; 3%nat).
 Definition r_2_loc : ChoiceEqualityLocation :=
+  (int64 ; 4%nat).
+Definition x_0_loc : ChoiceEqualityLocation :=
   (int64 ; 5%nat).
 Notation "'xor_inp'" :=(
   int64 '× int64 : choice_type) (in custom pack_type at level 2).
@@ -35,17 +35,13 @@ Notation "'xor_inp'" :=(int64 '× int64 : ChoiceEquality) (at level 2).
 Notation "'xor_out'" :=(int64 : choice_type) (in custom pack_type at level 2).
 Notation "'xor_out'" :=(int64 : ChoiceEquality) (at level 2).
 Definition XOR : nat :=
-  11.
+  8.
 Program Definition xor (x_inp_6 : int64) (y_inp_7 : int64)
   : both (CEfset ([x_0_loc ; y_1_loc ; r_2_loc])) [interface] (int64) :=
   ((letbm x_0 : int64 loc( x_0_loc ) := lift_to_both0 x_inp_6 in
       letbm y_1 : int64 loc( y_1_loc ) := lift_to_both0 y_inp_7 in
-      letb v_8 : int64 := lift_to_both0 x_0 in
-      letbm r_2 : int64 loc( r_2_loc ) := lift_to_both0 v_8 in
-      letb v1_9 : int64 := lift_to_both0 r_2 in
-      letb v2_10 : int64 := lift_to_both0 y_1 in
-      letbm r_2 loc( r_2_loc ) :=
-        (lift_to_both0 v1_9) .^ (lift_to_both0 v2_10) in
+      letbm r_2 : int64 loc( r_2_loc ) :=
+        (lift_to_both0 x_0) .^ (lift_to_both0 y_1) in
       lift_scope (H_loc_incl := _) (H_opsig_incl := _) (lift_to_both0 r_2)
       ) : both (CEfset ([x_0_loc ; y_1_loc ; r_2_loc])) [interface] (int64)).
 Fail Next Obligation.
--- examples/xor/src/xor.rs
+++ examples/xor/src/xor.rs
@@ -4,10 +4,6 @@ use hacspec_lib::*;
 fn xor(x_inp : u64, y_inp : u64) -> u64 {
     let mut x : u64 = x_inp;
     let mut y : u64 = y_inp;
-    let v = x;
-    let mut r : u64 = v;
-    let v1 = r;
-    let v2 = y;
-    r = v1 ^ v2;
+    let mut r : u64 = x ^ y;
     r
 }
