fcb0dabca8089348d926b7c364d30aab99c84d8a
Lasse Letager Hansen <lasse@letager.dk>
2023-02-03 14:26:59 +0100

    Fresh xor gen

--- coq_ssprove/src/Hacspec_Xor.v
+++ coq_ssprove/src/Hacspec_Xor.v
@@ -23,28 +23,26 @@ Open Scope hacspec_scope.
 Obligation Tactic := try timeout 8 solve_ssprove_obligations.
 
 
-Definition x_3412_loc : ChoiceEqualityLocation :=
-  (int64 ; 3415%nat).
-Definition y_3413_loc : ChoiceEqualityLocation :=
-  (int64 ; 3416%nat).
-Definition r_3414_loc : ChoiceEqualityLocation :=
-  (int64 ; 3417%nat).
+Definition r_2_loc : ChoiceEqualityLocation :=
+  (int64 ; 3%nat).
+Definition y_1_loc : ChoiceEqualityLocation :=
+  (int64 ; 4%nat).
+Definition x_0_loc : ChoiceEqualityLocation :=
+  (int64 ; 5%nat).
 Notation "'xor_inp'" :=(
   int64 '× int64 : choice_type) (in custom pack_type at level 2).
 Notation "'xor_inp'" :=(int64 '× int64 : ChoiceEquality) (at level 2).
 Notation "'xor_out'" :=(int64 : choice_type) (in custom pack_type at level 2).
 Notation "'xor_out'" :=(int64 : ChoiceEquality) (at level 2).
 Definition XOR : nat :=
-  3420.
-Program Definition xor (x_inp_3418 : int64) (y_inp_3419 : int64)
-  : both (CEfset ([x_3412_loc ; y_3413_loc ; r_3414_loc])) [interface] (
-    int64) :=
-  ((letbm x_3412 : int64 loc( x_3412_loc ) := lift_to_both0 x_inp_3418 in
-      letbm y_3413 : int64 loc( y_3413_loc ) := lift_to_both0 y_inp_3419 in
-      letbm r_3414 : int64 loc( r_3414_loc ) :=
-        (lift_to_both0 x_3412) .^ (lift_to_both0 y_3413) in
-      lift_scope (H_loc_incl := _) (H_opsig_incl := _) (lift_to_both0 r_3414)
-      ) : both (CEfset ([x_3412_loc ; y_3413_loc ; r_3414_loc])) [interface] (
-      int64)).
+  8.
+Program Definition xor (x_inp_6 : int64) (y_inp_7 : int64)
+  : both (CEfset ([x_0_loc ; y_1_loc ; r_2_loc])) [interface] (int64) :=
+  ((letbm x_0 : int64 loc( x_0_loc ) := lift_to_both0 x_inp_6 in
+      letbm y_1 : int64 loc( y_1_loc ) := lift_to_both0 y_inp_7 in
+      letbm r_2 : int64 loc( r_2_loc ) :=
+        (lift_to_both0 x_0) .^ (lift_to_both0 y_1) in
+      lift_scope (H_loc_incl := _) (H_opsig_incl := _) (lift_to_both0 r_2)
+      ) : both (CEfset ([x_0_loc ; y_1_loc ; r_2_loc])) [interface] (int64)).
 Fail Next Obligation.
 
