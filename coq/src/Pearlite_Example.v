6aff53f296474d49e018a2044cee86ba6826f6f2
Lasse Letager Hansen <cmester0@users.noreply.github.com>
2023-02-13 13:22:47 +0100

    Update Pearlite_Example.v

--- coq/src/Pearlite_Example.v
+++ coq/src/Pearlite_Example.v
@@ -16,18 +16,9 @@ Definition ensure_something
   : bool :=
   (x_2675) =.? (y_2676).
 
-<<<<<<< ../coq/src/Pearlite_Example.v
 Theorem ensures_ensure_something : forall result_2 (x_0 : int8) (y_1 : int8),
  forall {H_0 : x_0 = y_1},
  @ensure_something x_0 y_1 H_0 = result_2 ->
  ~ (result_2 = false).
  Proof. red ; intros. subst. cbn in *. now rewrite (eq_true y_1) in H0. Qed.
-=======
-Theorem ensures_ensure_something : forall result_2677 (x_2675 : int8) (
-  y_2676 : int8),
- forall {H_0 : x_2675 = y_2676},
- @ensure_something x_2675 y_2676 H_0 = result_2677 ->
- ~ (result_2677 = false).
- Proof. Admitted.
->>>>>>> ../coq/src/_temp/Pearlite_Example.v
 
