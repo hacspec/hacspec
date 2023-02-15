f20a897fcbdd51acfcc0d1ab007d257d46415cf1
Lasse Letager Hansen <lasse@letager.dk>
2023-02-15 15:20:40 +0100

    Cleanup

--- examples/aes_jazz/build.rs
+++
@@ -1,5 +0,0 @@
-// Example custom build script.
-fn main() {
-    // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rustc-link-search=/home/au538501/Documents/LocalHacspec/hacspec/examples/aes_jazz/src/jasmin");
-}
